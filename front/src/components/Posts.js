36d2f18984f19b1e0225d32a921cb2ff06763bd0
michellesagnelli-natgeo <michelle.sagnelli@natgeo.com>
2018-10-05 17:27:56 -0500

    feature (posts): add posts functionality using webServices file

--- front/src/components/Posts.js
+++ front/src/components/Posts.js
@@ -1,10 +1,37 @@
 import * as React from 'react'
+import { getPosts } from '../utils/webServices'
+import Post from './Post';
 
 class Posts extends React.PureComponent {
+  state = {
+    posts: [],
+    loading: false,
+  }
+
+  componentDidMount = async () => {
+    this.setState({ loading: true })
+    const posts = await getPosts();
+    this.setState({ posts, loading: false})
+  }
+
   render() {
+    const { posts, loading } = this.state
+
     return (
-      <div>
-        posts
+      <div className="posts-container">
+        <h1>Posts</h1>
+        {loading
+          ? (
+            <h3>Loading posts...</h3>
+          )
+          : posts.map((post, key) => (
+            <Post
+              post={post}
+              key={key}
+            />
+          ))
+
+        }
       </div>
     )
   }
--- front/src/utils/webServices.js
+++ front/src/utils/webServices.js
@@ -16,11 +16,11 @@ export const getPostComments = async (postId) => {
   await delay(2000);
   switch(postId) {
     case 1:
-      return COMMENTS.slice(0, 1);
+      return COMMENTS.slice(0, 2);
     case 2:
-      return COMMENTS.slice(2, 3);
+      return COMMENTS.slice(2, 4);
     case 3:
-      return COMMENTS.slice(4, 5);
+      return COMMENTS.slice(4, 6);
     default:
       return [];
   }
