d8019bd22972464699d1a535eb90ef1157fed191
Julian Orozco <orozco.castorena@outlook.com>
2018-11-01 21:14:20 -0600

    Add edit button

--- front/src/components/Post.js
+++ front/src/components/Post.js
@@ -1,9 +1,9 @@
-import * as React from 'react'
+import React, { Fragment, Component } from 'react'
 import Card from './Card'
 import { getPostComments } from '../utils/webServices'
 
-class Post extends React.PureComponent {
-    state = { 
+class Post extends Component {
+    state = {
         loading: false,
         comments: []
     }
@@ -20,36 +20,40 @@ class Post extends React.PureComponent {
 
 
         return (
-            <React.Fragment>
-                <Card
-                    children={
-                        <React.Fragment>
-                            <h3>{post.title}</h3>
-                            <p>{post.body}</p>
-                            <h5>by {post.user}</h5>
-                            <div className="comments-container">
-                                <button 
-                                    className="custom-button"
-                                    onClick={() => this.loadComments(post.id)}
-                                >
-                                    Comments
-                                </button>
-                                {loading
-                                  ? (
-                                    <h5 className="load-comments">Loading comments...</h5>
-                                  )
-                                  : comments.map((comment, key) => (
-                                    <Card
-                                        children={comment}
-                                        key={key}
-                                    />
-                                  ))
-                                }
-                            </div>
-                        </React.Fragment>
-                    }
-                />
-            </React.Fragment>
+            <Fragment>
+                <Card>
+                <Fragment>
+                    <h3>{post.title}</h3>
+                    <p>{post.body}</p>
+                    <h5>by {post.user}</h5>
+                    <button
+                        className="custom-button"
+                        onClick={() => this.loadComments(post.id)}
+                    >
+                        Edit
+                    </button>
+                    <button
+                        className="custom-button right"
+                        onClick={() => this.loadComments(post.id)}
+                    >
+                        Comments
+                    </button>
+                    <div className="comments-container">
+                        {loading
+                            ? (
+                            <h5 className="load-comments">Loading comments...</h5>
+                            )
+                            : comments.map((comment, key) => (
+                            <Card
+                                children={comment}
+                                key={key}
+                            />
+                            ))
+                        }
+                    </div>
+                </Fragment>
+                </Card>
+            </Fragment>
         )
     }
 }
