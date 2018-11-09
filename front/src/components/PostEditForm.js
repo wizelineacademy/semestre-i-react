ed0bdbe3c987b27ab7451ba171d05547eb3e68fd
chuyalvarez <jesusaagomez@hotmail.com>
2018-11-09 07:47:53 -0600

    updatePost mutation homework

--- front/src/components/PostEditForm.js
+++ front/src/components/PostEditForm.js
@@ -1,57 +1,82 @@
 import React, { Fragment, Component } from 'react';
-// import history from '../history';
+import { Mutation } from "react-apollo";
+import { UPDATE_POST } from '../graphql/mutations/posts';
+import history from '../history';
 
 class PostEditForm extends Component {
   constructor(props) {
     super(props);
+    const { id } = props.match.params;
     this.state = {
       title: '',
       content: '',
-      author: ''
+      author: '',
+      id
     };
   }
+
+  updateState = (event, key) => {
+    event.preventDefault();
+    this.setState({
+      [key]: event.target.value,
+    });
+  };
 
   submitPost = e => {
     e.preventDefault();
   }
 
   render() {
+    const { title, content, author } = this.state;
     return (
-      <Fragment>
-        <div className="posts-container">
-          <form>
-            <h1> Edit Post</h1>
-            <div className='form-container'>
-              <div>
-                <input
-                  type='text'
-                  placeholder='Author'
-                />
+      <Mutation mutation={UPDATE_POST} onCompleted={() => history.goBack()}>
+      {updatePost => (
+        <Fragment>
+          <div className="posts-container">
+            <form onSubmit={e => {
+                  e.preventDefault();
+                  updatePost({ variables: this.state });
+                }}>
+              <h1> Edit Post</h1>
+              <div className='form-container'>
+                <div>
+                  <input
+                    type='text'
+                    placeholder='Author'
+                    onChange={event => this.updateState(event, 'author')}
+                    value={author}
+                  />
+                </div>
+                <div>
+                  <input
+                    type='text'
+                    placeholder='Title'
+                    onChange={event => this.updateState(event, 'title')}
+                    value={title}
+                  />
+                </div>
+                <div>
+                  <textarea
+                    type='text'
+                    placeholder='Write the content...'
+                    onChange={event => this.updateState(event, 'content')}
+                    value={content}
+                  />
+                </div>
               </div>
               <div>
-                <input
-                  type='text'
-                  placeholder='Title'
-                />
+                <button
+                  className="custom-button"
+                  type='submit'
+                >
+                    Submit
+                </button>
               </div>
-              <div>
-                <textarea
-                  type='text'
-                  placeholder='Write the content...'
-                />
-              </div>
-            </div>
-            <div>
-              <button
-                className="custom-button"
-                onSubmit={() => this.submitPost()}
-              >
-                  Submit
-              </button>
-            </div>
-          </form>
-        </div>
-      </Fragment>
+            </form>
+          </div>
+        </Fragment>
+      )}
+      </Mutation>
     );
   }
 }
--- front/src/graphql/mutations/posts.js
+++ front/src/graphql/mutations/posts.js
@@ -10,5 +10,16 @@ const CREATE_POST = gql`
   }
 `;
 
+const UPDATE_POST = gql`
+  mutation updatePost($id: ID, $author: String!, $title: String, $content: String) {
+    updatePost(_id: $id,input: { author: $author, title: $title, content: $content }) {
+      author
+      title
+      content
+    }
+  }
+`;
+
 // eslint-disable-next-line
 export { CREATE_POST };
+export { UPDATE_POST };
